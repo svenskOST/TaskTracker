444a6e8bad7ee694532df26d1e28d9df4b7136d7
Alexander Marini <svenskost6@gmail.com>
2024-02-17 02:23:51 +0100

    better comment regarding basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      <BrowserRouter basename=''> {/*ändra basename inför produktion, t.ex. '/dist'*/}
+      {/*ändra basename inför produktion, t.ex. '/app-och-webb/arskurs-3/task-tracker'*/}
+      <BrowserRouter basename=''>
          <App />
       </BrowserRouter>
    </React.StrictMode>,
